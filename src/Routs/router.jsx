1217ae116c6201ef9652dabc948b6d0dd2e71ed7
rabiyarumi <rabiyaarumi@gmail.com>
2025-01-16 23:09:11 +0600

    added our menu pageA

--- src/Routs/router.jsx
+++ src/Routs/router.jsx
@@ -6,6 +6,7 @@ import MainLayout from "../layouts/MainLayout";
 import Home from "../Pages/Home/Home";
 import ContactUs from "../Pages/ContactUs/ContactUs";
 import Dashboard from "../Pages/Dashboard/Dashboard";
+import OurMenu from "../Pages/OurMenu/OurMenu";
 
 
   const router = createBrowserRouter([
@@ -24,7 +25,11 @@ import Dashboard from "../Pages/Dashboard/Dashboard";
         {
             path: "/dashboard",
             element: <Dashboard/>
-        }
+        },
+        {
+            path: "/menu",
+            element: <OurMenu/>
+        },
       ]
     },
   ]);
